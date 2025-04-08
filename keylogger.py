70f229e867a6bb6b4bc08e5b0dfce416fc2148f8
Jeff Shively <jshively@paloaltonetworks.com>
2025-04-08 08:48:47 -0400

    MVP

--- keylogger.py
+++ keylogger.py
@@ -1,18 +1,30 @@
-from pynput import keyboard
+import keyboard
+import time
 
-def on_press(key):
+def on_key_press(event):
+    """Function to handle key press events"""
+    # Print the name of the key that was pressed
+    print(f"Key pressed: {event.name}")
+
+    # If you want to capture special key combinations (like Ctrl+C)
+    if event.name == 'esc':
+        print("Escape key pressed - exiting program")
+        return False  # Returning False will stop the listener
+
+    return True  # Continue listening
+
+def main():
+    print("Keyboard input capture started. Press ESC to exit.")
+
+    # Register the callback function for key press events
+    keyboard.on_press(on_key_press)
+
+    # Keep the program running until ESC is pressed
     try:
-        print(key.char)
-    except AttributeError:
-        print(key.char)
+        keyboard.wait('esc')  # Wait until 'esc' key is pressed
+        print("Exiting the program.")
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
 
-def on_release(key):
-    print(key)
-
-
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release
-)
-
-listener.start()
+if __name__ == "__main__":
+    main()
